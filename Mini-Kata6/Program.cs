2c7ff23b726995cfb3ce522d9e32984781abb3db
Munterz <munterskevin@hotmail.com>
2024-11-13 18:09:16 +0100

    Mini-Kata 6

---
+++ Mini-Kata6/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        string[] enemies = { "Goblin", "Orc", "Troll" };
+        Console.WriteLine("Enemies:");
+        foreach (string enemy in enemies)
+        {
+            Console.WriteLine(enemy);
+        }
+        
+        List<string> inventory = new List<string> { "Sword", "Shield", "Potion" };
+        Console.WriteLine("\nPlayer Inventory:");
+        foreach (string item in inventory)
+        {
+            Console.WriteLine(item);
+        }
+        
+        inventory.Add("Helmet");
+        Console.WriteLine("\nUpdated Inventory:");
+        foreach (string item in inventory)
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
