fb3795d0994f35145634358acc4a9a533309dc98
Munterz <munterskevin@hotmail.com>
2024-11-13 17:48:51 +0100

    Kata 5

---
+++ Kata5/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        AttackEnemy("Goblin", 20);
+        HealPlayer("Arin", 15);
+    }
+
+    static void AttackEnemy(string enemyName, int damage)
+    {
+        Console.WriteLine($"Attacked {enemyName} and dealt {damage} damage!");
+    }
+
+    static void HealPlayer(string playerName, int healAmount)
+    {
+        Console.WriteLine($"Player {playerName} healed {healAmount} health points!");
+    }
+}
