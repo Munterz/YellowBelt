1b30a2da7c54d68757715400dd7b323763d58276
Munterz <munterskevin@hotmail.com>
2024-11-13 17:27:53 +0100

    Mini-Kata 5

---
+++ Mini-Kata5/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Attack(15);
+        Heal(10);
+    }
+
+    static void Attack(int damage)
+    {
+        Console.WriteLine($"Player dealt {damage} damage!");
+    }
+
+    static void Heal(int healAmount)
+    {
+        Console.WriteLine($"Player healed {healAmount} health points!");
+    }
+}
