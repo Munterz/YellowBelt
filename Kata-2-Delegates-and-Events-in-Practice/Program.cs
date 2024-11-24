8ff8422320d7f6147262302073c91116acc9cc7d
Munterz <munterskevin@hotmail.com>
2024-11-24 14:39:52 +0100

    Kata 2: Delegates and Events in Practice

---
+++ Kata-2-Delegates-and-Events-in-Practice/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+class Character
+{
+    public string Name { get; set; }
+    public int Health { get; private set; }
+    
+    public delegate void CharacterAction(Character target);
+    
+    public event Action HealthChanged;
+    
+    public Character(string name, int health)
+    {
+        Name = name;
+        Health = health;
+    }
+    
+    public void Attack(Character target)
+    {
+        Console.WriteLine($"{Name} attacks {target.Name}!");
+        target.ReduceHealth(10);
+    }
+    
+    private void ReduceHealth(int damage)
+    {
+        Health -= damage;
+        HealthChanged?.Invoke();
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Character warrior = new Character("Warrior", 100);
+        Character healer = new Character("Healer", 80);
+        
+        warrior.HealthChanged += () => Console.WriteLine($"{warrior.Name} now has {warrior.Health} health.");
+        healer.HealthChanged += () => Console.WriteLine($"{healer.Name} now has {healer.Health} health.");
+        
+        warrior.Attack(healer);
+        healer.Attack(warrior);
+    }
+}
